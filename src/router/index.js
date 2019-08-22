7df8223992fe3bb9f5e69c93b03d7db15babd9c6
Igor Batista Paiva <igorbatistapaiva@outlook.com>
2019-08-22 01:22:18 -0300

    Refactoring change vue router mode to history

--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,11 @@ import routes from './routes';
 
 Vue.use(VueRouter);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation
- */
-
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
-
-    // Leave these as is and change from quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: 'history',
+    mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE,
   });
 
